ede050699f269497cd621b8f170429640ed4442c
edwinmah <me@edwinmah.com>
2017-01-06 07:43:25 -0500

    Create store

---
+++ client/src/js/store.jsx
@@ -0,0 +1,7 @@
+import { redux, createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/index';
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+
+export default store;
