7a483a68377cc86dcafe9d0263df3c8f1fd837a8
edwinmah <me@edwinmah.com>
2017-01-06 07:39:32 -0500

    Implement Redux, React-Router

--- client/src/js/index.jsx
+++ client/src/js/index.jsx
@@ -3,10 +3,18 @@ require('../css/style.css');
 require('babel-polyfill');
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { redux, Provider } from 'react-redux';
+import { router, Router, Route, hashHistory } from 'react-router';
+import store from './store';
+import App from './components/App';
 
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <h1>Hello world...</h1>,
+    <Provider store={store}>
+      <Router history={hashHistory}>
+        <Route path="/" component={App} />
+      </Router>
+    </Provider>,
     document.getElementById('react-app'));
 });
