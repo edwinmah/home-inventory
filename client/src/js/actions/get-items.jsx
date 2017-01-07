75f9c5e3bc05492f1d0fa6dc8ee1837846586ead
edwinmah <me@edwinmah.com>
2017-01-07 09:53:26 -0500

    Create get-items action

---
+++ client/src/js/actions/get-items.jsx
@@ -0,0 +1,56 @@
+require('isomorphic-fetch');
+
+
+const FETCH_ITEMS_SUCCESS = 'FETCH_ITEMS_SUCCESS';
+const fetchItemsSuccess = (items) => {
+  return {
+    type: FETCH_ITEMS_SUCCESS,
+    items: items
+  };
+};
+
+
+const FETCH_ITEMS_ERROR = 'FETCH_ITEMS_ERROR';
+const fetchItemsError = (items, error) => {
+  return {
+    type: FETCH_ITEMS_ERROR,
+    items: items,
+    error: error
+  };
+};
+
+
+const fetchItems = (items) => {
+  return (dispatch) => {
+    const init = { method: 'GET' };
+    const url  = '/items';
+
+    return fetch(url, init).then((response) => {
+      if (response.status < 200 || response.status >= 300) {
+        const error = new Error(response.statusText);
+        error.response = response;
+        throw error;
+      }
+      return response;
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const items = data;
+        return dispatch(fetchItemsSuccess(items));
+      })
+      .catch((error) => {
+        return dispatch(fetchItemsError(items, error));
+      });
+  }
+};
+
+
+exports.FETCH_ITEMS_SUCCESS  = FETCH_ITEMS_SUCCESS;
+exports.fetchItemsSuccess    = fetchItemsSuccess;
+
+exports.FETCH_ITEMS_ERROR  = FETCH_ITEMS_ERROR;
+exports.fetchItemsError    = fetchItemsError;
+
+exports.fetchItems  = fetchItems;
