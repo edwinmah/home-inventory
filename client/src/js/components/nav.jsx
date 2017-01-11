082d02d913ef2ccb2bf8730cc4082e895f16d361
edwinmah <me@edwinmah.com>
2017-01-11 14:05:22 -0500

    Use links

--- client/src/js/components/nav.jsx
+++ client/src/js/components/nav.jsx
@@ -6,27 +6,15 @@ import CategoriesList from './categories-list';
 class Nav extends React.Component {
   constructor(props) {
     super(props);
-    this.handleClickForItems = this.handleClickForItems.bind(this);
-    this.handleClickForAcctInfo = this.handleClickForAcctInfo.bind(this);
-  }
-
-  handleClickForItems() {
-    console.log('change current view to all items');
-    // dispatch action to change current view to all items
-  }
-
-  handleClickForAcctInfo() {
-    console.log('change current view to account info');
-    // dispatch action to change current view to account info (owner and policy details)
   }
 
   render() {
     return (
       <nav>
         <ul>
-          <li><Link to={'/items'} onClick={this.handleClickForItems}>All Items</Link></li>
+          <li><Link to={'/items'}>All Items</Link></li>
           <CategoriesList />
-          <li onClick={this.handleClickForAcctInfo}>Account Info</li>
+          <li><Link to={'/account'}>Account Info</Link></li>
         </ul>
       </nav>
     );
