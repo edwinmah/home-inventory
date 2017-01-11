dce91eed267966bba6f22bcd092065f222e39bc8
edwinmah <me@edwinmah.com>
2017-01-11 14:03:19 -0500

    Add routes

--- client/src/js/index.jsx
+++ client/src/js/index.jsx
@@ -7,13 +7,20 @@ import { redux, Provider } from 'react-redux';
 import { router, Router, Route, hashHistory } from 'react-router';
 import store from './store';
 import App from './components/App';
+import AllItems from './components/items-list';
+import AcctInfo from './components/acct-info';
+
 
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <Provider store={store}>
       <Router history={hashHistory}>
-        <Route path="/" component={App} />
+        <Route path="" component={App}>
+          <Route path="/" component={AllItems} />
+          <Route path="/items" component={AllItems} />
+          <Route path="/account" component={AcctInfo} />
+        </Route>
       </Router>
     </Provider>,
     document.getElementById('react-app'));
