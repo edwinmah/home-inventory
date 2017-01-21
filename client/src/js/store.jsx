03a1d0c7a8fa07675e712c26424d23af5d26e275
edwinmah <me@edwinmah.com>
2017-01-21 12:14:15 -0500

    Remove unneeded import

--- client/src/js/store.jsx
+++ client/src/js/store.jsx
@@ -1,4 +1,4 @@
-import { redux, createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index';
 const store = createStore(rootReducer, applyMiddleware(thunk));
