ca6a84673d45d77e2b341e1424d373233b7b685e
edwinmah <me@edwinmah.com>
2017-01-23 15:50:09 -0500

    Start building create-item action

---
+++ client/src/js/actions/create-item.jsx
@@ -0,0 +1,73 @@
+require('isomorphic-fetch');
+import { hashHistory } from 'react-router';
+import actions from './get-single-item';
+import actionsAll from '../actions/get-items';
+import actionsSingle from '../actions/get-single-item';
+
+
+var CREATE_ITEM_SUCCESS = 'CREATE_ITEM_SUCCESS';
+var createItemSuccess = function(item) {
+  return {
+    type: CREATE_ITEM_SUCCESS,
+    item: item
+  };
+};
+
+
+var CREATE_ITEM_ERROR = 'CREATE_ITEM_ERROR';
+var createItemError = function(item, error) {
+  return {
+    type: CREATE_ITEM_ERROR,
+    item: item,
+    error: error
+  };
+};
+
+
+var createItem = function(obj) {
+  var myHeaders = new Headers();
+  myHeaders.append('Content-Type', 'application/json');
+  return function(dispatch) {
+    var init = {
+      method: 'POST',
+      body: JSON.stringify(obj),
+      headers: myHeaders
+    };
+    var url  = '/item';
+
+    return fetch(url, init).then(function(response) {
+      if (response.status < 200 || response.status >= 300) {
+        var error = new Error(response.statusText);
+        error.response = response;
+        throw error;
+      }
+      return response;
+    })
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(data) {
+        var item = data;
+        return dispatch(createItemSuccess(item));
+      })
+      .then(function() {
+        dispatch(actionsAll.fetchItems());
+        dispatch(actionsSingle.fetchSingleItem(item._id));
+        hashHistory.push(`${url}/${item._id}`);
+        document.body.scrollTop = 0;
+      })
+      .catch(function(error) {
+        console.log(error);
+        return dispatch(createItemError(error));
+      });
+  }
+};
+
+
+exports.CREATE_ITEM_SUCCESS = CREATE_ITEM_SUCCESS;
+exports.createItemSuccess   = createItemSuccess;
+
+exports.CREATE_ITEM_ERROR = CREATE_ITEM_ERROR;
+exports.createItemError   = createItemError;
+
+exports.editItem  = editItem;
