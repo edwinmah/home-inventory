522afe4e50c27504677e045b837463b7521d2116
edwinmah <me@edwinmah.com>
2017-01-23 22:01:17 -0500

    Add select input for categories

--- client/src/js/components/edit-item.jsx
+++ client/src/js/components/edit-item.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { router, Link } from 'react-router';
 import { connect } from 'react-redux';
 import actions from '../actions/edit-item';
+import actionsCreate from '../actions/create-item';
 import DropzoneS3Uploader from 'react-dropzone-s3-uploader';
 import Datetime from 'react-datetime';
 
@@ -28,7 +29,7 @@ class EditItem extends React.Component {
       {
         _id: this.props.currentItem._id,
         ownerId: this.props.currentItem.ownerId,
-        categoryId: this.props.currentItem.categoryId,
+        categoryId: this.refs.category.value,
         name: this.refs.name.value,
         replaceValue: parseInt(this.refs.replaceValue.value),
         notes: this.refs.notes.value,
@@ -73,8 +74,16 @@ class EditItem extends React.Component {
     }, 2000);
   }
 
+  renderCategoryNames(categoryId) {
+    const { name } = this.props.categories[categoryId];
+
+    return (
+      <option key={categoryId} id={`item-${categoryId}`} value={categoryId}>{name}</option>
+    );
+  }
+
   render() {
-    const { _id, name, serialNumber, notes, replaceValue, purchaseDate, placePurchased, receipt, image } = this.props.currentItem;
+    const { _id, categoryId, name, serialNumber, notes, replaceValue, purchaseDate, placePurchased, receipt, image } = this.props.currentItem;
 
     const style = { backgroundColor: '#ffffff'};
     const activeStyle = { backgroundColor: '#fbf1a9' };
@@ -84,6 +93,7 @@ class EditItem extends React.Component {
 
     const dateInputProps = {
       id: 'purchaseDate',
+      name: 'purchaseDate',
       className: 'db w-100 pa2 input-reset ba b--black-20 br2 sans-serif'
     };
 
@@ -96,19 +106,24 @@ class EditItem extends React.Component {
         </div>
         <form className="flex flex-column w-100 w-50-ns f5" onSubmit={this.handleSubmit}>
           <label htmlFor="name" className="b db mb2">Name:</label>
-          <input type="text" id="name" className="db input-reset ba b--black-20 br2 pa2 mb3 sans-serif" defaultValue={name} ref="name" />
+          <input type="text" name="name" id="name" className="db input-reset ba b--black-20 br2 pa2 mb3 sans-serif" defaultValue={name} ref="name" />
 
           <label htmlFor="replacementValue" className="b db mb2">Replacement Value:</label>
-          <input type="text" id="replacementValue" className="db input-reset ba b--black-20 br2 pa2 mb3 sans-serif" defaultValue={replaceValue} ref="replaceValue" />
+          <input type="text" name="replacementValue" id="replacementValue" className="db input-reset ba b--black-20 br2 pa2 mb3 sans-serif" defaultValue={replaceValue} ref="replaceValue" />
 
           <label htmlFor="serialNumber" className="b db mb2">Serial Number:</label>
-          <input type="text" id="serialNumber" className="db input-reset ba b--black-20 br2 pa2 mb3 sans-serif" defaultValue={serialNumber} ref="serialNumber" />
+          <input type="text" name="serialNumber" id="serialNumber" className="db input-reset ba b--black-20 br2 pa2 mb3 sans-serif" defaultValue={serialNumber} ref="serialNumber" />
+
+          <label htmlFor="category" className="b db mb2">Category:</label>
+          <select name="category" id="category" className="pa2 mb3 sans-serif" ref="category">
+            {Object.keys(this.props.categories).map((categoryId) => this.renderCategoryNames(categoryId))}
+          </select>
 
           <label htmlFor="purchaseDate" className="b db mb2">Purchase Date:</label>
           <Datetime closeOnSelect={true} timeFormat={false} dateFormat='ddd, MMM Do YYYY' inputProps={dateInputProps} className="mb3" defaultValue={purchaseDate} ref="purchaseDate" />
 
           <label htmlFor="purchasePlace" className="b db mb2">Place Purchased:</label>
-          <input type="text" id="purchasePlace" className="db input-reset ba b--black-20 br2 pa2 mb3 sans-serif" defaultValue={placePurchased} ref="placePurchased" />
+          <input type="text" name="purchasePlace" id="purchasePlace" className="db input-reset ba b--black-20 br2 pa2 mb3 sans-serif" defaultValue={placePurchased} ref="placePurchased" />
 
           <label htmlFor="receiptUpload" className="b db mb2">Receipt:</label>
           <DropzoneS3Uploader onFinish={this.handleRecUpload} style={style} activeStyle={activeStyle} multiple={false} maxFileSize={1024*1024*50} s3Url="https://homeinventorybucket.s3.amazonaws.com" className="flex items-center justify-center relative h3 h4-l b--dashed bw1 b--black-20 br2 pointer">
@@ -116,7 +131,7 @@ class EditItem extends React.Component {
           </DropzoneS3Uploader>
 
           <label htmlFor="notes" className="b db mt3 mb2">Notes:</label>
-          <textarea id="notes" className="db border-box hover-black w-100 vh-25 measure ba b--black-20 pa2 br2 mb3 sans-serif" defaultValue={notes} ref="notes"></textarea>
+          <textarea name="notes" id="notes" className="db border-box hover-black w-100 vh-25 measure ba b--black-20 pa2 br2 mb3 sans-serif" defaultValue={notes} ref="notes"></textarea>
 
           <div className="flex flex-row">
             <Link to={`/item/${_id}`} className="w-50 f5 link bn br2 ph3 pv2 mr2 mv3 white bg-mid-gray hover-bg-dark-gray sans-serif tc">Cancel</Link>
@@ -140,6 +155,7 @@ EditItem.defaultProps = {
 
 const mapStateToProps = (state, props) => {
   return {
+    categories: state.categories,
     items: state.items,
     currentItem: state.items[props.params.id]
   }
