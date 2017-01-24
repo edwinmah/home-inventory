700da70ece62dfb79c738ca8229786768b3eace9
edwinmah <me@edwinmah.com>
2017-01-24 14:39:59 -0500

    Add renderHeading and adjust style classes

--- client/src/js/components/edit-item.jsx
+++ client/src/js/components/edit-item.jsx
@@ -91,6 +91,12 @@ class EditItem extends React.Component {
     );
   }
 
+  renderHeading() {
+    if (!this.props.params.id) {
+      return <h2 className="pv3">Add an item</h2>;
+    }
+  }
+
   render() {
     const { _id, categoryId, name, serialNumber, notes, replaceValue, purchaseDate, placePurchased, receipt, image } = this.props.currentItem;
 
@@ -105,50 +111,53 @@ class EditItem extends React.Component {
       name: 'purchaseDate',
       className: 'db w-100 pa2 input-reset ba b--black-20 br2 sans-serif'
     };
-console.log(this.props);
+
     return (
-      <div className="flex flex-column flex-row-ns">
-        <div className="w-100 w-50-ns mb3 mb0-ns mr4-ns">
-          <DropzoneS3Uploader onFinish={this.handleImgUpload} style={style} activeStyle={activeStyle} multiple={false} maxFileSize={1024*1024*50} s3Url="https://homeinventorybucket.s3.amazonaws.com" className="flex items-center justify-center relative overflow-hidden vh-25 vh-50-l b--dashed bw1 b--black-20 br2 pointer">
-            <img src={image} alt={name} className="h-auto w-75 nested-img img br2" />
-          </DropzoneS3Uploader>
-          <p>{uploadImgMsg}</p>
-        </div>
-        <form className="flex flex-column w-100 w-50-ns f5" onSubmit={this.handleSubmit}>
-          <label htmlFor="name" className="b db mb2">Name:</label>
-          <input type="text" name="name" id="name" className="db input-reset ba b--black-20 br2 pa2 mb3 sans-serif" defaultValue={name} ref="name" />
-
-          <label htmlFor="replacementValue" className="b db mb2">Replacement Value:</label>
-          <input type="text" name="replacementValue" id="replacementValue" className="db input-reset ba b--black-20 br2 pa2 mb3 sans-serif" defaultValue={replaceValue} ref="replaceValue" />
-
-          <label htmlFor="serialNumber" className="b db mb2">Serial Number:</label>
-          <input type="text" name="serialNumber" id="serialNumber" className="db input-reset ba b--black-20 br2 pa2 mb3 sans-serif" defaultValue={serialNumber} ref="serialNumber" />
-
-          <label htmlFor="category" className="b db mb2">Category:</label>
-          <select name="category" id="category" className="pa2 mb3 sans-serif" ref="category">
-            {Object.keys(this.props.categories).map((categoryId) => this.renderCategoryNames(categoryId))}
-          </select>
-
-          <label htmlFor="purchaseDate" className="b db mb2">Purchase Date:</label>
-          <Datetime closeOnSelect={true} timeFormat={false} dateFormat='ddd, MMM Do YYYY' inputProps={dateInputProps} className="mb3" defaultValue={purchaseDate} ref="purchaseDate" />
-
-          <label htmlFor="purchasePlace" className="b db mb2">Place Purchased:</label>
-          <input type="text" name="purchasePlace" id="purchasePlace" className="db input-reset ba b--black-20 br2 pa2 mb3 sans-serif" defaultValue={placePurchased} ref="placePurchased" />
-
-          <label htmlFor="receiptUpload" className="b db mb2">Receipt:</label>
-          <DropzoneS3Uploader onFinish={this.handleRecUpload} style={style} activeStyle={activeStyle} multiple={false} maxFileSize={1024*1024*50} s3Url="https://homeinventorybucket.s3.amazonaws.com" className="flex items-center justify-center relative h3 h4-l b--dashed bw1 b--black-20 br2 pointer">
-            <img src={image} alt={name} className="h-auto w-25 nested-img img br2" />
-          </DropzoneS3Uploader>
-          <p className="ph2">{uploadRecMsg}</p>
-
-          <label htmlFor="notes" className="b db mt3 mb2">Notes:</label>
-          <textarea name="notes" id="notes" className="db border-box hover-black w-100 vh-25 measure ba b--black-20 pa2 br2 mb3 sans-serif" defaultValue={notes} ref="notes"></textarea>
-
-          <div className="flex flex-row">
-            <Link to={`/item/${_id}`} className="w-50 f5 link bn br2 ph3 pv2 mr2 mv3 white bg-mid-gray hover-bg-dark-gray sans-serif tc">Cancel</Link>
-            <button type="submit" className="w-50 f5 link bn br2 ph3 pv2 ml2 mv3 white bg-dark-blue hover-bg-navy sans-serif">Save</button>
+      <div className="mw6 mw8-ns center ph3">
+        {this.renderHeading()}
+        <div className="flex flex-column flex-row-ns">
+          <div className="w-100 w-50-ns mb3 mb0-ns mr4-ns">
+            <DropzoneS3Uploader onFinish={this.handleImgUpload} style={style} activeStyle={activeStyle} multiple={false} maxFileSize={1024*1024*50} s3Url="https://homeinventorybucket.s3.amazonaws.com" className="flex items-center justify-center relative overflow-hidden vh-25 vh-50-l b--dashed bw1 b--black-20 br2 pointer">
+              <img src={image} alt={name} className="h-auto w-75 nested-img img br2" />
+            </DropzoneS3Uploader>
+            <p>{uploadImgMsg}</p>
           </div>
-        </form>
+          <form className="flex flex-column w-100 w-50-ns f5" onSubmit={this.handleSubmit}>
+            <label htmlFor="name" className="b db mb2">Name:</label>
+            <input type="text" name="name" id="name" className="db input-reset ba b--black-20 br2 pa2 mb3 sans-serif" defaultValue={name} ref="name" />
+
+            <label htmlFor="replacementValue" className="b db mb2">Replacement Value:</label>
+            <input type="text" name="replacementValue" id="replacementValue" className="db input-reset ba b--black-20 br2 pa2 mb3 sans-serif" defaultValue={replaceValue} ref="replaceValue" />
+
+            <label htmlFor="serialNumber" className="b db mb2">Serial Number:</label>
+            <input type="text" name="serialNumber" id="serialNumber" className="db input-reset ba b--black-20 br2 pa2 mb3 sans-serif" defaultValue={serialNumber} ref="serialNumber" />
+
+            <label htmlFor="category" className="b db mb2">Category:</label>
+            <select name="category" id="category" className="pa2 mb3 sans-serif" ref="category">
+              {Object.keys(this.props.categories).map((categoryId) => this.renderCategoryNames(categoryId))}
+            </select>
+
+            <label htmlFor="purchaseDate" className="b db mb2">Purchase Date:</label>
+            <Datetime closeOnSelect={true} timeFormat={false} dateFormat='ddd, MMM Do YYYY' inputProps={dateInputProps} className="mb3" defaultValue={purchaseDate} ref="purchaseDate" />
+
+            <label htmlFor="purchasePlace" className="b db mb2">Place Purchased:</label>
+            <input type="text" name="purchasePlace" id="purchasePlace" className="db input-reset ba b--black-20 br2 pa2 mb3 sans-serif" defaultValue={placePurchased} ref="placePurchased" />
+
+            <label htmlFor="receiptUpload" className="b db mb2">Receipt:</label>
+            <DropzoneS3Uploader onFinish={this.handleRecUpload} style={style} activeStyle={activeStyle} multiple={false} maxFileSize={1024*1024*50} s3Url="https://homeinventorybucket.s3.amazonaws.com" className="flex items-center justify-center relative h3 h4-l b--dashed bw1 b--black-20 br2 pointer">
+              <img src={image} alt={name} className="h-auto w-25 nested-img img br2" />
+            </DropzoneS3Uploader>
+            <p className="ph2">{uploadRecMsg}</p>
+
+            <label htmlFor="notes" className="b db mt3 mb2">Notes:</label>
+            <textarea name="notes" id="notes" className="db border-box hover-black w-100 vh-25 measure ba b--black-20 pa2 br2 mb3 sans-serif" defaultValue={notes} ref="notes"></textarea>
+
+            <div className="flex flex-row">
+              <Link to={`/item/${_id}`} className="w-50 f5 link bn br2 ph3 pv2 mr2 mv3 white bg-mid-gray hover-bg-dark-gray sans-serif tc">Cancel</Link>
+              <button type="submit" className="w-50 f5 link bn br2 ph3 pv2 ml2 mv3 white bg-dark-blue hover-bg-navy sans-serif">Save</button>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
