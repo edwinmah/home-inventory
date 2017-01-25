9a790055556b69748515d4cc9acc8c91cc9bd72a
edwinmah <me@edwinmah.com>
2017-01-25 14:50:36 -0500

    Add delete case

--- client/src/js/reducers/items-reducer.jsx
+++ client/src/js/reducers/items-reducer.jsx
@@ -1,4 +1,7 @@
 import actions from '../actions/get-items';
+import { FETCH_SINGLE_ITEM_SUCCESS } from '../actions/get-single-item';
+import { EDIT_ITEM_SUCCESS } from '../actions/edit-item';
+import { DELETE_SINGLE_ITEM_SUCCESS } from '../actions/delete-item';
 
 
 const initialState = {};
@@ -14,14 +17,21 @@ const AllItems = (state, action) => {
       return Object.assign({}, state, newItems);
       break;
 
-    case actions.FETCH_SINGLE_ITEM_SUCCESS :
+    case FETCH_SINGLE_ITEM_SUCCESS :
       const item = action.item;
       return Object.assign({}, state, { [item._id]: item });
       break;
 
-    case actions.EDIT_ITEM_SUCCESS :
+    case EDIT_ITEM_SUCCESS :
       const editedItem = action.item;
       return Object.assign({}, state, { [editedItem._id]: editedItem });
+      break;
+
+    case DELETE_SINGLE_ITEM_SUCCESS :
+      const deletedItem = action.itemId;
+      const newState = Object.assign({}, state);
+      delete newState[deletedItem];
+      return newState;
       break;
 
     default :
