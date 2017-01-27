d58b1b2d56c326c76328ad17b899d649d91b4247
edwinmah <me@edwinmah.com>
2017-01-27 13:05:44 -0500

    Add Google auth keys

--- server/config.js
+++ server/config.js
@@ -4,4 +4,6 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
    'mongodb://localhost/home-inventory' :
    'mongodb://localhost/home-inventory-dev');
 
+exports.GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+exports.GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 exports.PORT = process.env.PORT || 8080;
