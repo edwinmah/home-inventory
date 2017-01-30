73a250849d84246a0717318597511ddb2c71729b
edwinmah <me@edwinmah.com>
2017-01-30 17:03:37 -0500

    Refactor to use fetchAuth; remove duplicate action

--- client/src/js/actions/create-item.jsx
+++ client/src/js/actions/create-item.jsx
@@ -1,7 +1,6 @@
-require('isomorphic-fetch');
+import fetchAuth from '../fetchAuth';
 import { hashHistory } from 'react-router';
 import actions from './get-single-item';
-import actionsSingle from '../actions/get-single-item';
 
 
 var CREATE_ITEM_SUCCESS = 'CREATE_ITEM_SUCCESS';
@@ -23,42 +22,21 @@ var createItemError = function(item, error) {
 };
 
 
-var createItem = function(obj) {
-  var myHeaders = new Headers();
-  myHeaders.append('Content-Type', 'application/json');
-  return function(dispatch) {
-    var init = {
-      method: 'POST',
-      body: JSON.stringify(obj),
-      headers: myHeaders
-    };
-    var url  = '/item';
-
-    return fetch(url, init).then(function(response) {
-      if (response.status < 200 || response.status >= 300) {
-        var error = new Error(response.statusText);
-        error.response = response;
-        throw error;
-      }
-      return response;
-    })
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(data) {
-        var item = data;
-        return dispatch(createItemSuccess(item));
-      })
-      .then(function(data) {
-        dispatch(actionsSingle.fetchSingleItem(data.item._id));
-        hashHistory.push(`${url}/${data.item._id}`);
-        document.body.scrollTop = 0;
-      })
-      .catch(function(error) {
-        console.log(error);
-        return dispatch(createItemError(error));
-      });
-  }
+var createItem = (obj) => (dispatch, obj) => {
+  fetchAuth('POST', '/item', obj)
+  .then(function(data) {
+    var item = data;
+    return dispatch(createItemSuccess(item));
+  })
+  .then(function(data) {
+    dispatch(actions.fetchSingleItem(data.item._id));
+    hashHistory.push(`${url}/${data.item._id}`);
+    document.body.scrollTop = 0;
+  })
+  .catch(function(error) {
+    console.log(error);
+    return dispatch(createItemError(error));
+  });
 };
 
 
