45516565889969bff25e6c83960fce0bb23cd3e5
edwinmah <me@edwinmah.com>
2017-01-30 11:05:29 -0500

    Adjust style classes for add/edit

--- client/src/js/components/nav.jsx
+++ client/src/js/components/nav.jsx
@@ -30,7 +30,7 @@ class Nav extends React.Component {
           <li className="relative pa3 nested-list-reset dark-blue hover-navy pointer" onClick={this.handleMenuDisplay}>Categories <span dangerouslySetInnerHTML={dropDownIcon}></span>
             <ul className={`absolute left-0 z-9999 mt3 bt bw1 b--dark-blue br2 shadow-1 ${dropDownDisplay}`}>
               <CategoriesList />
-              <li className="db pa3 bt b--black-20 dark-blue hover-navy link bg-light-gray hover-bg-light-silver">Add/Edit</li>
+              <li><Link to={'/categories'} className="db pa3 bt b--black-20 dark-blue hover-navy link bg-light-gray hover-bg-light-silver">Add/Edit</Link></li>
             </ul>
           </li>
           <li className="pa3"><Link to={'/account'} className="dark-blue hover-navy link">Account Info</Link></li>
