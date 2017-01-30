2d43b50f554e6f4df57f03ba8b3124fb8bc95754
edwinmah <me@edwinmah.com>
2017-01-30 13:30:48 -0500

    Create fetchAuth function

---
+++ client/src/js/fetchAuth.js
@@ -0,0 +1,26 @@
+require('isomorphic-fetch');
+import cookie from 'react-cookie';
+
+
+export default function fetchAuth(method, url) {
+  var myHeaders = new Headers();
+  myHeaders.append('Content-Type', 'application/json');
+  myHeaders.append('Authorization', `Bearer ${cookie.load('accessToken')}`);
+
+  const init = {
+    method: method,
+    headers: myHeaders
+  }
+
+  return fetch(url, init).then((response) => {
+    if (response.status < 200 || response.status >= 300) {
+      const error = new Error(response.statusText);
+      error.response = response;
+      throw error;
+    }
+    return response;
+  })
+    .then((response) => {
+    return response.json();
+  })
+}
