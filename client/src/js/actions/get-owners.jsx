733cdeba9e9b0ecf0edc6b8a3bce41712ce3bb15
edwinmah <me@edwinmah.com>
2017-01-30 13:31:24 -0500

    Refactor to use fetchAuth

--- client/src/js/actions/get-owners.jsx
+++ client/src/js/actions/get-owners.jsx
@@ -1,5 +1,4 @@
-require('isomorphic-fetch');
-import cookie from 'react-cookie';
+import fetchAuth from '../fetchAuth';
 
 
 const FETCH_OWNERS_SUCCESS = 'FETCH_OWNERS_SUCCESS';
@@ -21,36 +20,15 @@ const fetchOwnersError = (owners, error) => {
 };
 
 
-const fetchOwners = () => {
-  var myHeaders = new Headers();
-  myHeaders.append('Content-Type', 'application/json');
-  myHeaders.append('Authorization', `Bearer ${cookie.load('accessToken')}`);
-  return (dispatch) => {
-    const init = {
-      method: 'GET',
-      headers: myHeaders
-    };
-    const url  = '/owners';
-
-    return fetch(url, init).then((response) => {
-      if (response.status < 200 || response.status >= 300) {
-        const error = new Error(response.statusText);
-        error.response = response;
-        throw error;
-      }
-      return response;
-    })
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        const owners = data;
-        return dispatch(fetchOwnersSuccess(owners));
-      })
-      .catch((error) => {
-        return dispatch(fetchOwnersError(error));
-      });
-  }
+const fetchOwners = () => dispatch => {
+  fetchAuth('GET', '/owners')
+  .then((data) => {
+    const owners = data;
+    return dispatch(fetchOwnersSuccess(owners));
+  })
+  .catch((error) => {
+    return dispatch(fetchOwnersError(error));
+  });
 };
 
 
