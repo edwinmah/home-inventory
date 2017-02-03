43764e79b485330842ada33641e7d0233839b145
edwinmah <me@edwinmah.com>
2017-02-03 18:10:12 -0500

    Refactor definition lists rendering

--- client/src/js/components/policies-list.jsx
+++ client/src/js/components/policies-list.jsx
@@ -10,6 +10,48 @@ class PoliciesList extends React.Component {
     super(props);
   }
 
+  renderWebsiteLink(policyId) {
+    return <a href={`${this.props.policies[policyId].website}`} className="dark-blue hover-navy link">{this.props.policies[policyId].website}</a>;
+  }
+
+  renderEmailLink(policyId) {
+    return <a href={`mailto:${this.props.policies[policyId].email}`} className="dark-blue hover-navy link">{this.props.policies[policyId].email}</a>;
+  }
+
+  renderDefinitionLists(property, i) {
+    const policyId = Object.keys(this.props.policies)[0];
+    let ddValue, dtValue;
+    switch (property) {
+      case 'policyNumber' :
+        dtValue = `${property.slice(0, 6)} ${property.slice(-6)}`;
+        ddValue = '';
+        break;
+      case 'coverage' :
+        dtValue = property;
+        ddValue = `${formatAsCurrency(`${this.props.policies[policyId][property]}`)}`;
+        break;
+      case 'website' :
+        dtValue = property;
+        ddValue = this.renderWebsiteLink(policyId);
+        break;
+      case 'email' :
+        dtValue = property;
+        ddValue = this.renderEmailLink(policyId);
+        break;
+      default :
+        dtValue = property;
+        ddValue = this.props.policies[policyId][property];
+        break;
+    }
+
+    return (
+      <dl key={`${i}-${policyId}`} className="flex lh-title mv1">
+        <dt className="mr2 b ttc">{dtValue}:</dt>
+        <dd className="ml0 dark-gray">{ddValue}</dd>
+      </dl>
+    );
+  }
+
   renderPolicies(policyId) {
     const { company, coverage } = this.props.policies[policyId];
     const keys = Object.keys(this.props.policies[policyId]).filter((property) => {
@@ -20,18 +62,7 @@ class PoliciesList extends React.Component {
       <article key={policyId} id={`item-${policyId}`}>
         <h3>{company}</h3>
         <div className="flex flex-column">
-          <div>
-            {keys.map((property, i) => {
-              return (
-                <dl key={`${i}-${policyId}`} className="flex lh-title mv2">
-                  <dt className="mr2 b ttc">{(property === 'policyNumber') ? `${property.slice(0, 6)} ${property.slice(-6)}` : property}:</dt>
-                  <dd className="ml0 dark-gray">
-                    {(property === 'coverage') ? `${formatAsCurrency(`${coverage}`)}` : `${this.props.policies[policyId][property]}`}
-                  </dd>
-                </dl>
-              );
-            })}
-          </div>
+          {keys.map((property, i) => this.renderDefinitionLists(property, i))}
           <Link to={`account/policy/edit/${policyId}`} className="w-50 w-25-l link br2 ph3 pv2 mv3 white bg-dark-blue hover-bg-navy tc">Edit Policy</Link>
         </div>
       </article>
