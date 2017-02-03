c2b313007548e65d33fcc91861fbbe31d1401b6f
edwinmah <me@edwinmah.com>
2017-02-03 12:47:22 -0500

    Refactor to export functions directly

--- client/src/js/utils.jsx
+++ client/src/js/utils.jsx
@@ -1,19 +1,15 @@
-const formatAsCurrency = (number) => {
+export const formatAsCurrency = (number) => {
   return `$${number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}`;
 }
 
-const calcTotalValue = (array, obj, property)  => {
+export const calcTotalValue = (array, obj, property)  => {
   return array.map((i) => obj[i][property]).reduce((a,b) => a + b);
 }
 
-const sanitizeNumber = (string) => {
+export const sanitizeNumber = (string) => {
   if (string === '' || string === null || string === undefined) {
     string = '0';
   }
   return parseInt(string.replace(/[`_+-.,!@#$%^&*();\/|<>"']/g, ''));
 }
-
-exports.formatAsCurrency = formatAsCurrency;
-exports.calcTotalValue   = calcTotalValue;
-exports.sanitizeNumber   = sanitizeNumber;
 
