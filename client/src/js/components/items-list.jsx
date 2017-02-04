fdf97e289ff0a0d256fd8ad0e77f9f9e272ab8f9
edwinmah <me@edwinmah.com>
2017-02-04 14:00:47 -0500

    Remove unneeded variable import

--- client/src/js/components/items-list.jsx
+++ client/src/js/components/items-list.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { router, Link } from 'react-router';
+import { Link } from 'react-router';
 import { connect } from 'react-redux';
 import { formatAsCurrency, calcTotalValue } from '../utils';
 
