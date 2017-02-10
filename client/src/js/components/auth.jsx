8efcd978efb49d778c6cf9ce94ba2ee03445d9c4
edwinmah <me@edwinmah.com>
2017-02-09 21:30:34 -0500

    Adjust markup and styling of login

--- client/src/js/components/auth.jsx
+++ client/src/js/components/auth.jsx
@@ -10,11 +10,15 @@ class GoogleAuthorization extends React.Component {
 
   render() {
     return (
-      <section className="flex flex-column items-center mw6 center">
-        <h2>Welcome</h2>
-        <p className="mb4 lh-copy">Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Etiam porta sem malesuada magna mollis euismod. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
-        <GoogleButton />
-      </section>
+      <article className="mw6 mw8-ns center">
+        <header className="mb4 bt bb b--black-20">
+          <h2 className="mw6 center fw3 f4 tracked">Hello and welcome...</h2>
+        </header>
+        <div className="flex flex-column items-center">
+          <p className="w-100 w-50-ns mb4 lh-copy">Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Etiam porta sem malesuada magna mollis euismod. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
+          <GoogleButton />
+        </div>
+      </article>
     );
   }
 }
